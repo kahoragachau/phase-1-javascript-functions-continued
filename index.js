016e5dfca5d7f87c45569a11d6acd505f55c333d
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-18 15:07:24 +0300

    - Add comment for better understanding

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function saturdayFun(activity ="roller-skate"){
 function mondayWork(activity = "go to the office"){
     return `This Monday, I will ${activity}.`
 }
+// purpose of the below function is that the inner function can access the outer function elements/variables
 function wrapAdjective(result){
     const emphatic = function(option="a hard worker"){
-        // console.log(`You are ${option}${result}${option}!`)
         return `You are ${result}${option}${result}!`
     }
     return emphatic
